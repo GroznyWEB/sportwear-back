f93c1b55f3d8a7d4f50cdc2709cd2df9ea93f449
KhEsanukaev <halid.esanukaev.86@mail.ru>
2025-04-28 11:40:05 +0300

    fix/сделал изменения в моделе, роуте и контроллере

--- routes/products.route.js
+++ routes/products.route.js
@@ -1,12 +1,11 @@
-const { Router } = require('express')
-const { productsController } = require('../controllers/products.controller')
+const { Router } = require('express');
+const { productsController } = require('../controllers/products.controller');
 
-const router = Router()
+const router = Router();
 
-router.get('/Product', productsController.getProducts)
-router.post('/Product', productsController.addProducts)
-router.delete('/Product',productsController.deleteProducts)
+router.get('/product', productsController.getProducts); 
+router.get('/product/:id', productsController.getProductById);
+router.post('/product', productsController.addProducts); 
+router.delete('/product/:id', productsController.deleteProducts); 
 
-
-
-module.exports = router
+module.exports = router;
